74afe0f2c0511f59f9aa4f956cfbb06f001db520
Ovi T. (Impelon) <ovi.tatar@googlemail.com>
2018-03-06 23:45:14 +0100

    Added main Arduino-File

---
+++ che4c-arduino/che4c-arduino.ino
@@ -0,0 +1,44 @@
+/**
+ * Hauptdatei des Programms af dem Arduino. Wartet auf Befehle über die serielle Schnittstelle.
+ */
+
+static const int BUFFER_LENGTH = 33;
+
+/**
+ * Funktion, welche bei Ausführung des Programms zuerst ausgeführt wird.
+ */
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  randomSeed(analogRead(0));
+
+  // SChFiM initialisieren
+  setBaseSpeed(150);
+  resetMotors();
+  // SUIfM initialisieren
+  reset();
+}
+
+/**
+ * Funktion, welche nach dem Startvorgang wiederholt ausgeführt wird.
+ */
+void loop() {
+  // SUIfM-Interaktion
+  interact();
+}
+
+/**
+ * Funktion, welche ausgeführt wird, falls Daten über die serielle Schnittstelle empfangen werden können.
+ * Liest eine Zeile und gibt diese zur Interpretation durch SChFiM und SUIfM weiter.
+ */
+void serialEvent() {
+  char input[BUFFER_LENGTH];
+  
+  int endIndex = Serial.readBytesUntil('\n', input, BUFFER_LENGTH - 2);
+
+  input[endIndex] = '\n';
+  input[endIndex + 1] = '\0';
+
+  SChFiM_interpretLines(input);
+  SUIfM_interpretLines(input);
+}
